18ecd59f1f75927fa62345c6e3de73f5ef11211f
최예원 <choiyewon127@gmail.com>
2021-09-22 16:29:03 +0900

    layout edit

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,19 +2,23 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+//import android.widget.ImageView
 import android.widget.Toast
+import com.example.helloandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var imageView: ImageView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_main)
     }
 
     fun onClick(view: android.view.View) {
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        imageView.setImageResource(R.drawable.drawing)
+        binding.imageView.setImageResource(R.drawable.drawing)
 
         Toast.makeText( this, "이미지를 보여줍니다.", Toast.LENGTH_LONG).show()
     }
